b08996849027b4082f4e102dff6f1dddd009db5f
Alex Heiner <alexgheiner@gmail.com>
2021-04-02 20:43:17 -0600

    started schema

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,12 +8,39 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-const mongoose = require('mongoose');
 
 // connect to the database
 mongoose.connect('mongodb://localhost:27017/dream-team', {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
+const playerSchema = new mongoose.Schema({
+    team: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Team',
+    },
+    name: String,
+    team: String,
+    position: String,
+    stats: {
+        fieldGoal: Number,
+        assists: Number,
+        rebounds: Number,
+        steals: Number
+    },
+    rating: Number,
+    image: String,
+});
+
+const teamSchema = new mongoose.Schema({
+    teamName: String,
+    team: String,
+});
+
+const Player = mongoose.model('Player', playerSchema);
+
+// Model for items
+const Team = mongoose.model('Team',teamSchema);
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
